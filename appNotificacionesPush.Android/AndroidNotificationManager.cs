0c1ef48d05c9dfaa62cc968991f6c3728550b1c2
Alfonso Gutierrez <fonsuspro@hotmail.com>
2020-08-08 12:45:31 -0500

    Agregar archivos de proyecto.

---
+++ appNotificacionesPush.Android/AndroidNotificationManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using AndroidApp = Android.App.Application;
+
+namespace appNotificacionesPush.Droid
+{
+    public class AndroidNotificationManager
+    {
+        const string channelId = "default";
+        const string channelName = "Default";
+        const string channelDescription = "The default channel for notifications.";
+        const int pendingIntentId = 0;
+
+        public const string TitleKey = "title";
+        public const string MessageKey = "message";
+
+        bool channelInitialized = false;
+        int messageId = -1;
+        NotificationManager manager;
+
+
+        void CreateNotificationChannel()
+        {
+            manager = (NotificationManager)AndroidApp.Context.GetSystemService(AndroidApp.NotificationService);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                var channelNameJava = new Java.Lang.String(channelName);
+                var channel = new NotificationChannel(channelId, channelNameJava, NotificationImportance.Default)
+                {
+                    Description = channelDescription
+                };
+                manager.CreateNotificationChannel(channel);
+            }
+
+            channelInitialized = true;
+        }
+
+        public int CrearNotificacionLocal(string pTitle, string pBody)
+        {
+
+            if (!channelInitialized)
+            {
+                CreateNotificationChannel();
+            }
+
+            messageId++;
+
+            Intent intent = new Intent(AndroidApp.Context, typeof(MainActivity));
+            intent.PutExtra(TitleKey, pTitle);
+            intent.PutExtra(MessageKey, pBody);
+            intent.AddFlags(ActivityFlags.ClearTop);
+
+            PendingIntent pendingIntent = PendingIntent.GetActivity(AndroidApp.Context, pendingIntentId, intent, PendingIntentFlags.UpdateCurrent);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(AndroidApp.Context, channelId)
+                .SetContentIntent(pendingIntent)
+                .SetContentTitle(pTitle)
+                .SetContentText(pBody)
+                .SetAutoCancel(true)
+                .SetLargeIcon(BitmapFactory.DecodeResource(AndroidApp.Context.Resources, Resource.Drawable.abc_btn_check_material))
+                .SetSmallIcon(Resource.Drawable.abc_btn_check_material)
+                .SetDefaults((int)NotificationDefaults.Sound | (int)NotificationDefaults.Vibrate);
+
+            var notification = builder.Build();
+            manager.Notify(messageId, notification);
+
+            return messageId;
+        }
+
+    }
+}
---
+++ appNotificacionesPush.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Util;
+using Firebase.Messaging;
+using Xamarin.Essentials;
+
+namespace appNotificacionesPush.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        AndroidNotificationManager androidNotification = new AndroidNotificationManager();
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+            androidNotification.CrearNotificacionLocal(message.GetNotification().Title, message.GetNotification().Body);
+
+        }
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+
+            Preferences.Set("TokenFirebase", token);
+            sedRegisterToken(token);
+        }
+        public void sedRegisterToken(string token) { 
+            //Tu código para registrar el token a tu servidor y base de datos
+        }
+    }
+}
